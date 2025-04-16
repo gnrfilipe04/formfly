dad8030f3954b5513e53b583b1bae26df9fb27ee
gnrfilipe04 <gnr_filipe@hotmail.com>
2025-04-16 17:31:39 -0300

    add two pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,41 +19,26 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
+    <Tabs screenOptions={{
+      headerSearchBarOptions: {},
+      animation: 'shift',
+      popToTopOnBlur: true,
+      tabBarStyle: {
+        paddingTop: 10,
+        height: 100
+      }
+    }}>
+      <Tabs.Screen 
+        name="(home)" 
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
+          title: 'Ordens',
+          tabBarIcon: ({ color }) => <TabBarIcon name="th-list" color={color} />
+        }} 
       />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
+      <Tabs.Screen name="(two)" options={{ 
+        title: 'Apontamentos', 
+        tabBarIcon: ({ color }) => <TabBarIcon name="file-text" color={color} />
+      }} />
     </Tabs>
   );
 }
